82db03adb2c2aacd3b147b70ac33267cd3687889
Netshroud <netshroud@gmail.com>
2016-07-09 23:08:07 +1000

    Create ApplicationException implementation, guarded behind !APPLICATIONEXCEPTION_AVAILABLE. ApplicationException is not available in .NET Core.

---
+++ SteamKit2/SteamKit2/Util/ApplicationException.cs
@@ -0,0 +1,20 @@
+﻿#if !APPLICATIONEXCEPTION_AVAILABLE
+namespace System
+{
+#if BINARY_SERIALIZATION_AVAILABLE
+    [Serializable]
+#endif
+    public class ApplicationException : Exception
+    {
+        public ApplicationException() { }
+        public ApplicationException(string message) : base(message) { }
+        public ApplicationException(string message, Exception inner) : base(message, inner) { }
+
+#if BINARY_SERIALIZATION_AVAILABLE
+        protected ApplicationException(
+          Runtime.Serialization.SerializationInfo info,
+          Runtime.Serialization.StreamingContext context) : base(info, context) { }
+#endif
+    }
+}
+#endif
